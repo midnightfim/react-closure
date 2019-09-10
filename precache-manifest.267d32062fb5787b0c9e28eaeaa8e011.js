ae4997d24e03434d11bb9c2873b734a1feecaa3f
Yaroslav <y.karmannikov@yandex.ru>
2019-09-10 23:14:36 +0300

    Updates

---
+++ precache-manifest.267d32062fb5787b0c9e28eaeaa8e011.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb1fd1dfd4d137a852bf0ddd3bb17694",
+    "url": "/react-closure/index.html"
+  },
+  {
+    "revision": "f71fecf917f00f43b6c8",
+    "url": "/react-closure/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2fc5113be0da51329a41",
+    "url": "/react-closure/static/js/2.c7bfc1d3.chunk.js"
+  },
+  {
+    "revision": "f71fecf917f00f43b6c8",
+    "url": "/react-closure/static/js/main.fcc1d8f9.chunk.js"
+  },
+  {
+    "revision": "a97a857558c1904c7221",
+    "url": "/react-closure/static/js/runtime~main.79444811.js"
+  }
+]);
